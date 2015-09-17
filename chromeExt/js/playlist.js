560faf14a7b8cbed59529802479c58f0393f8b46
Rattan Priya <bhasin.rattanpriya@gmail.com>
2015-09-17 14:32:18 -0700

    added a toggle method for play/pause in the list

--- chromeExt/js/playlist.js
+++ chromeExt/js/playlist.js
@@ -1,12 +1,32 @@
-window.onload = init();
-function init() {
-	document.getElementById("play_clicks").onclick = function() {
-		document.getElementsByTagName("ol")[0].style.visibility = "visible"
-		var data = queue;	
-		for (var i = 0; i < queue.length; i++ ) {
-			var click = queue[i];
-			console.log(document.elementFromPoint(click.X, click.Y));		
-		}
-	
-	}	
-}
+window.onload = Player();
+
+function Player() {
+	this.state = ['stop', 'play', 'pause'];
+
+	function init() {
+		document.getElementById("play_clicks").onclick = function() {
+			this.state = togglePlay();
+			var data = queue;	
+			var list = document.getElementById("click-list");
+			list.textContent = '';
+
+			for (var i = queue.length-1; i >= 0; i-- ) {
+				var click = queue[i];
+				var item = document.createElement("li");
+				//<li>Blessing it <span class="time">3:24</span></li>
+		        var textnode = document.createTextNode(click.X + " " +click.Y);
+		        item.appendChild(textnode);         
+		        list.appendChild(item);                     
+		        console.log(document.elementFromPoint(click.X, click.Y));		
+			}
+		
+		}	
+
+	}
+
+	function setState() {
+		this.state = !this.state;
+		return this.state;
+	}
+
+};
