c0b40a4828d668eed09984debe4beab3fb1d9d11
Matin Fouladi <matin.fouladi@appdynamics.com>
2015-09-17 03:14:47 -0700

    Updated readme with instructions for the server and cleaned up the server file.

--- Server/server.js
+++ Server/server.js
@@ -1,6 +1,6 @@
 var express      = require('express'),
-	app	           = express(),
-	bodyParser 		 = require('body-parser'),
+  app            = express(),
+  bodyParser     = require('body-parser'),
   cookieParser   = require('cookie-parser'),
   session        = require('express-session'),
   morgan         = require('morgan');
@@ -17,8 +17,6 @@ app.use(cookieParser());
 app.use(bodyParser());
 app.use(session({ secret : 'keyboard cat'}));
 
-
-//
 app.use('/js', express.static(__dirname+'/client/js'));
 app.use('/bower_components', express.static(__dirname+'/bower_components'));
 app.use('/css', express.static(__dirname+'/client/views/css'));
@@ -31,7 +29,7 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/client/views/home.html');
 });
 
-//access with
+//Can pass data through url
 //http://localhost:3000/api/test?name=matin&birth=yes&death=no
 app.get('/api/test', function(req, res){
   testdb.insert(req.query, function(err, result) {
@@ -48,7 +46,7 @@ app.get('/api/test', function(req, res){
   });
 });
 
-//access with
+//Can post data with ajax call
 //$.ajax({type:"POST",url:"http://localhost:3000/api/testPost", data:{hello: "goodbye"}, success:null, dataType:"json"});
 //$.post("http://localhost:3000/api/testPost",{test:"test"}, function(data){console.log(data);}, "json");
 app.post('/api/testPost', function(req, res){
@@ -64,7 +62,7 @@ app.post('/api/testPost', function(req, res){
   });
 });
 
-//get everything from database
+//Can pull everything from the database
 app.get('/api/all', function(req, res){
   testdb.find().toArray(function(err, result){
     if (err) {
@@ -78,17 +76,8 @@ app.get('/api/all', function(req, res){
       res.json({});
     }
   });
-})
-
-
-//Conversation Calls
-// app.get('/api/conversation/inactive', conversationController.listInactiveConversations);
-// app.get('/api/conversation/active', conversationController.listActiveConversations);
-// app.post('/api/conversation/create', conversationController.createConversation);
-// app.get('/api/conversation/activate/:phoneNumber', conversationController.activateConversation);
-// app.get('/api/conversation/deactivate/:phoneNumber', conversationController.deactivateConversation);
-// app.get('/api/conversation/open/:phoneNumber', conversationController.openConversation);
+});
 
 app.listen(3000, function(){
-	console.log('I\'m Listening...');
+  console.log('I\'m Listening...');
 });
